4342f1593b97e488cf2d87c945deb46abc999609
Rachma Adzima Maulida <rachmadzima4@gmail.com>
2024-05-22 17:33:41 +0700

    fix: add condition API URL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const accessToken = Cookies.get('accessToken');
+const getCurrentUrl = () => {
+  const url = window.location.href;
+
+  if (url.includes('localhost')) return 'http://localhost:3000';
+  return 'https://ganf-backend.vercel.app';
+};
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${getCurrentUrl()}/api`,
   headers: {
     'Content-Type': 'multipart/form-data',
     authorization: accessToken ? `Bearer ${accessToken}` : '',
